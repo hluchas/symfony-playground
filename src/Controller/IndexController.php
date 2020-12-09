2680d106870794ad4e620a58ccce8de045a82a67
Lukas Drahy <lukas@drahy.net>
2020-12-09 19:37:21 +0100

    Add save mechanism

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\Type\UserType;
 use App\Repository\UserRepository;
-use App\Service\ExampleService;
+use App\Service\UserService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,22 +16,21 @@ class IndexController extends AbstractController
     /**
      * @Route("/", name="index")
      */
-    public function index(Request $request, ExampleService $service): Response
+    public function index(Request $request, UserService $userService): Response
     {
         $entity = new User();
         $form = $this->createForm(UserType::class, $entity);
 
         /** @var UserRepository $repository */
 //        $repository = $this->getDoctrine()->getRepository(User::class);
-//        $service->test();
 
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            /** @var User $data */
-            $data = $form->getData();
 
-            throw new \RuntimeException('POST not implemented');
+            $userService->save($form->getData());
+
+            die('TY!');
         }
 
         return $this->render('index/index.html.twig', [
---
+++ src/Service/UserService.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * @copyright   2020 Mautic Contributors. All rights reserved
+ * @author      Mautic
+ *
+ * @link        https://mautic.org
+ *
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+namespace App\Service;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
+
+class UserService
+{
+    /**
+     * @var UserRepository
+     */
+    private $repository;
+
+    /**
+     * @var EntityManager
+     */
+    private $entityManager;
+
+    /**
+     * @param UserRepository $repository
+     * @param EntityManager $entityManager
+     */
+    public function __construct(UserRepository $repository, EntityManager $entityManager)
+    {
+        $this->repository = $repository;
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @param User $user
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function save(User $user): void
+    {
+        $this->entityManager->persist($user);
+        $this->entityManager->flush($user);
+    }
+}
